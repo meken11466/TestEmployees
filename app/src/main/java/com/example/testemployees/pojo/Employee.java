20f1bbe8d42716c8d2062cc401858727026ffa6d
TestUser <test>
2021-09-08 12:31:05 +0300

    fill Employee.java

--- app/src/main/java/com/example/testemployees/pojo/Employee.java
+++ app/src/main/java/com/example/testemployees/pojo/Employee.java
@@ -1,5 +1,65 @@
 package com.example.testemployees.pojo;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+
+import java.util.List;
+
 public class Employee {
+    @SerializedName("f_name")
+    @Expose
+    private String fName;
+    @SerializedName("l_name")
+    @Expose
+    private String lName;
+    @SerializedName("birthday")
+    @Expose
+    private String birthday;
+    @SerializedName("avatr_url")
+    @Expose
+    private String avatrUrl;
+    @SerializedName("specialty")
+    @Expose
+    private List<Speciality> specialty = null;
+
+    public String getfName() {
+        return fName;
+    }
+
+    public void setfName(String fName) {
+        this.fName = fName;
+    }
+
+    public String getlName() {
+        return lName;
+    }
+
+    public void setlName(String lName) {
+        this.lName = lName;
+    }
+
+    public String getBirthday() {
+        return birthday;
+    }
+
+    public void setBirthday(String birthday) {
+        this.birthday = birthday;
+    }
+
+    public String getAvatrUrl() {
+        return avatrUrl;
+    }
+
+    public void setAvatrUrl(String avatrUrl) {
+        this.avatrUrl = avatrUrl;
+    }
+
+    public List<Speciality> getSpecialty() {
+        return specialty;
+    }
+
+    public void setSpecialty(List<Speciality> specialty) {
+        this.specialty = specialty;
+    }
 
 }
